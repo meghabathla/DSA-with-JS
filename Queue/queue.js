95b1342dab55cf52fb5eb5fb51de9555fe2950da
Megha-Bathla <bathlamegha6@gmail.com>
2024-07-10 20:58:00 +0530

    queue in js

---
+++ Queue/queue.js
@@ -0,0 +1,56 @@
+//Note: - enqueue dequeue
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(data) {
+    this.queue.push(data);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.queue.shift();
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.queue[0];
+  }
+
+  size() {
+    return this.queue.length;
+  }
+
+  clear() {
+    this.queue = [];
+  }
+
+  printQueue() {
+    let str = "";
+    for (let i = 0; i < this.queue.length; i++) {
+      str += this.queue[i] + "\n";
+    }
+    return str;
+  }
+}
+
+//usage Example
+
+const myqueue = new Queue();
+
+myqueue.enqueue(3);
+myqueue.enqueue(9);
+myqueue.enqueue(7);
+myqueue.enqueue(6);
+
+myqueue.dequeue();
+console.log(myqueue.printQueue());
