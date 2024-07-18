7cae9e821f9949b83644d4f4ac178e0b453a34c5
Megha-Bathla <bathlamegha6@gmail.com>
2024-07-18 18:42:19 +0530

    insertion

--- LinkedList/DoublyLinkedlist.js
+++ LinkedList/DoublyLinkedlist.js
@@ -12,7 +12,44 @@ class DoublyLinkedList {
     this.tail = null;
   }
 }
-
+//insert at the begining
 DoublyLinkedList.prototype.insertInBeginning = function (data) {
-  const newNode = new Node(data);
+  const newNode = new Node(null, data, this.head);
+  if (this.head !== null) {
+    thiss.head.prev = newNode;
+  }
+  this.head = newNode;
+  if (this.head === null) {
+    this.head = newNode;
+  }
+};
+
+// insert at the End
+DoublyLinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = new Node(this.tail, data, null);
+
+  if (this.tail !== null) {
+    this.tail.next = newNode;
+  }
+  this.tail = newNode;
+  if (this.tail === null) {
+    this.tail = null;
+  }
+};
+
+//insert in between
+DoublyLinkedList.prototype.insertInBetween = function (prevNode, data) {
+  if (prevNode === null) {
+    return `Invalid prev Node`;
+  }
+
+  const newNode = new Node(prevNode, data, prevNode.next);
+  if (prevNode !== null) {
+    prevNode.next.prev = newNode;
+  }
+  prevNode.next = newNode;
+
+  if (newNode == tail) {
+    this.tail = newNode;
+  }
 };
