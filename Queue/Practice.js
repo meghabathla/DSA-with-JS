1430d9a203a2b567f66395c79cbaea146a36037e
Megha-Bathla <bathlamegha6@gmail.com>
2024-09-22 00:59:21 +0530

    Practice queue done

---
+++ Queue/Practice.js
@@ -0,0 +1,50 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(data) {
+    this.queue.push(data);
+  }
+  dequeue() {
+    if (this.isEmpty()) {
+      return "queue is empty";
+    } else {
+      return this.queue.shift();
+    }
+  }
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return "queue is empty";
+    } else {
+      return this.queue[0];
+    }
+  }
+  size() {
+    return this.queue.length;
+  }
+  clear() {
+    return (this.queue = []);
+  }
+  printQueue() {
+    let str = "";
+    for (let i = 0; i < this.queue.length; i++) {
+      str += this.queue[i] + "\n";
+    }
+    return str;
+  }
+}
+
+const result = new Queue();
+result.enqueue(90);
+result.enqueue(76);
+result.enqueue(34);
+result.enqueue(23);
+console.log(result.isEmpty());
+console.log(result.printQueue());
+console.log(result.peek());
+console.log(result.size());
+console.log(result.dequeue());
+console.log(result.printQueue());
