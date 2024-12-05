65cb4fae9090cb57e7ea6ecff008076da66e7353
Megha <bathlamegha6@gmail.com>
2024-12-05 18:39:16 +0530

    day 3 challenge complete

--- Nitesh_Array_string_questions/emozi.js
+++ Nitesh_Array_string_questions/emozi.js
@@ -1,3 +1,16 @@
+/*  Santas Emoji Hack!
+
+During Christmas, Santa wants to ban negative emojis, so when people
+use negative emoji shortcodes, he wants positive emojis to appear instead.
+
+In other words, :angry: should result in 🎁 instead of 😠.
+
+
+*/
+const inputPhrase = document.getElementById("input");
+const button = document.getElementById("button");
+const output = document.getElementById("show_emoji");
+
 const hackedEmojis = {
   angry: "🎁", // 😠
   thumbsdown: "👏", // 👎
@@ -6,27 +19,62 @@ const hackedEmojis = {
   puke: "🤩", // 🤮
 };
 
+/* 1. Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, check if it exists in the hackedEmojis object, 
+and replace it with the corresponding emoji. If not, return the original word.
+
+
+Example input: ":cry:"
+Example output: ‍😄
+
+*/
+const EmojiObject = {
+  "😠": "🎁",
+  "👎": "👏",
+  "🤦‍♂️": "🎅",
+  "😭": "‍😄",
+  "🤮": "🤩",
+};
+
 function emojifyWord(word) {
+  //:angry:
   console.log(word);
-  condition = word?.startsWith(":") && word.endsWith(":");
-  console.log(condition);
+  let condition = word.startsWith(":") && word.endsWith(":");
   if (condition) {
-    updatedWord = word.replace(/:/g, "");
-    console.log(updatedWord);
-    for (const emozi in hackedEmojis) {
-      if (emozi === updatedWord) return hackedEmojis[emozi];
+    let updatedWord = word.replace(/:/g, "");
+    if (hackedEmojis[updatedWord]) {
+      return hackedEmojis[updatedWord];
     }
-    return word;
+  } else if (EmojiObject[word]) {
+    //"😠"
+    return EmojiObject[word];
   }
+  return word;
 }
+
+// console.log(emojifyWord(":angry:"));
+
+/* 2. Write a function to find any emoji shortcodes in a phrase.
+Use your emojify function from the previous exercise!
+
+Example input: "Just read your article :thumbsdown:"
+Example output: "Just read your article 👏"
+*/
 
 function emojifyPhrase(phrase) {
   const regex = /:\w+:/g;
-  const getEmozi = phrase.match(regex);
-
-  if (!getEmozi) return phrase;
-  const newString = getEmozi.join("");
-  return emojifyWord(newString);
+  const matched = phrase.match(regex); //null
+  if (!matched) return emojifyWord(phrase);
+  const emoji = matched.join("");
+  return emojifyWord(emoji);
 }
 
-console.log(emojifyPhrase("Those shoes puke"));
+// console.log(emojifyPhrase("Those shoes :puke:"));
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  output.innerHTML = emojifyPhrase(inputPhrase.value);
+});
+
+// Stretch goal: don't just replace the shortcodes, but also
+// any emojis are added directly to the text.
